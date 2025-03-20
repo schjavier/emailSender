4b62eba76b08fecc79ca1a1672e8ff1450cf9c0f
javier <schjavier@gmail.com>
2025-03-20 16:36:47 -0300

    (add) main class of email sender

---
+++ src/main/java/com/lotorojo/emailSender/controller/ContactFormController.java
@@ -0,0 +1,41 @@
+package com.lotorojo.emailSender.controller;
+
+import com.lotorojo.emailSender.model.ContactForm;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/send")
+public class ContactFormController {
+
+    JavaMailSender mailSender;
+
+    public ContactFormController(JavaMailSender javaMailSender){
+        this.mailSender = javaMailSender;
+    }
+
+
+    @PostMapping("/{addressee}")
+    public ResponseEntity<String> enviarCorreo (@RequestBody ContactForm contactForm,
+                                                @PathVariable String addressee){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(addressee);
+        message.setFrom("emailsender@lotorojo.com.ar");
+        message.setSubject(contactForm.getSubject());
+        message.setReplyTo(contactForm.getEmail());
+        message.setText(contactForm.getName() +
+                " Paso por tu web y te dejó este mensaje: " +
+                contactForm.getText());
+
+        mailSender.send(message);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Correo enviado");
+    }
+
+
+}
