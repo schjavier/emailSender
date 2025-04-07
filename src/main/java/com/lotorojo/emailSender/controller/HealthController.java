6165b028ee8a322269525a582d0f3b5ba676c0cd
javier <schjavier@gmail.com>
2025-04-07 06:12:26 -0300

    (add) healthController

---
+++ src/main/java/com/lotorojo/emailSender/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.lotorojo.emailSender.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("Service is ip and running");
+    }
+
+}
