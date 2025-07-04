7408cf41f82cf0c013a98227cc67f72caca36328
Javier <schjavier@gmail.com>
2025-07-03 22:37:31 -0300

    add: application.properties for test with H2 memeory database

--- src/test/java/com/lotorojo/emailSender/EmailSenderApplicationTests.java
+++ src/test/java/com/lotorojo/emailSender/EmailSenderApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class EmailSenderApplicationTests {
 
 	@Test
