0592aab05ad0f45249a650b71ceafdb9ff7dd161
Javier <schjavier@gmail.com>
2026-01-03 13:17:31 -0300

    fix: contextLoads test false positive

--- src/test/java/com/lotorojo/emailSender/EmailSenderApplicationTests.java
+++ src/test/java/com/lotorojo/emailSender/EmailSenderApplicationTests.java
@@ -1,13 +1,15 @@
 package com.lotorojo.emailSender;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class EmailSenderApplicationTests {
+
+    @MockitoBean
+    private JavaMailSender mailSender;
 
 	@Test
 	void contextLoads() {
